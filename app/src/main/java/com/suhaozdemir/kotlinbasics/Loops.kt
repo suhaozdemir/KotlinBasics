8e9d985c1b289471cf3f6506b6545b6d1863d1f8
suhaozdemir <ahmetsuhaozdemir@gmail.com>
2022-07-18 11:32:35 +0300

    Added Functions.kt Corrected Loops.kt

--- app/src/main/java/com/suhaozdemir/kotlinbasics/Loops.kt
+++ app/src/main/java/com/suhaozdemir/kotlinbasics/Loops.kt
@@ -1,59 +1,58 @@
 package com.suhaozdemir.kotlinbasics
 
 fun main(){
-    // Control Flows
-    // If Statements
-    val age = 17
-    if(age >= 21){
-        print("now you may drink in the US")
-    }
-    // Else If Statement - only executed if the if statement is not true
-    else if(age >= 18){
-        print("now you may vote")
-    }
-    // Else If Statement - only executed if the foregoing else if statement is not true
-    else if (age >= 16){
-        print("you now may drive")
-    }
-    // else is only executed if all of the foregoing statements weren't true
-    else{
-        print("you're too young")
+    var condition = true
+    // Loops
+    // While Loop
+    // While loop executes a block of code repeatedly as long as a given condition is true
+    while(condition) {
+        // code to be executed
     }
 
-    // Kotlin’s "when" expression is the replacement of the switch statement
-    // from other languages like C, C++, and Java.
-    // It is compact and more powerful than switch statements.
-    val season = 3
-    when(season) {
-        1 -> println("Spring")
-        2 -> println("Summer")
-        3 -> println("Fall")
-        4 -> println("Winter")
-        else -> println("Invalid Season")
-    }
-    val month = 3
-    when(month) {
-        1,2,3 -> println("Spring")
-        in 4..6 -> println("Summer")
-        in 7..9 -> println("Fall")
-        in 10..12 -> println("Winter")
-        else -> println("Invalid Season")
+    var y = 1
+    while(y <= 10) {
+        println("$y ")
+        y++
     }
 
-    // challenge - translate the if statement with the age to a when expression
-    when(age){
-        // with the !in it's the same as saying not in ...
-        !in 0..20  -> print("now you may drink in the US")
-        in 18..20  -> print("now you may vote")
-        16,17 -> print("you now may drive")
-        else -> print("you're too young")
+    // Do while loop
+    // The do-while loop is similar to while loop except that it
+    // tests the condition at the end of the loop.
+    // This means that it will at least execute the body once
+    var z = 1
+    do {
+        print("$z ")
+        z++
+    } while(z <= 10)
+
+    var feltTemp = "cold"
+    var roomTemp = 10
+    while (feltTemp == "cold"){
+        roomTemp++
+        if(roomTemp == 20){
+            feltTemp = "comfy"
+            println("it's comfy now")
+        }
     }
 
-    val x : Any = 13.37
-    when(x) {
-        is Int -> println("$x is an Int")
-        !is Double -> println("$x is not Double")
-        is String -> println("$x is a String")
-        else -> println("$x is none of the above")
+
+    // For Loop
+    // A for-loop is used to iterate through ranges, arrays, collections, or anything
+    // that provides an iterator (You’ll learn about iterator, arrays, ranges and collections in a future lectur    e).
+    for(num in 1..10) {
+        print("$num ")
+    }
+
+    // infix notation
+    for(i in 1 until 10) { // Same as - for(i in 1.until(10))
+        print("$i ")
+    }
+
+    for(i in 10 downTo 1) {     // Same as - for(i in 10.downTo(1))
+        print("$i ")
+    }
+
+    for(i in 1 until 10 step 2) { // Same as - for(i in 1.until(10).step(2))
+        print("$i ")
     }
 }
